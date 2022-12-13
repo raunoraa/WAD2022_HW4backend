6a2066e89873a392dc600089d629813b9d057933
Rauno Raa <rauno.raa@ut.ee>
2022-12-13 19:44:25 +0200

    probably ready

--- database.js
+++ database.js
@@ -44,7 +44,7 @@ const createTblQuery2 = `
     CREATE TABLE IF NOT EXISTS "posttable" (
 	    "id" SERIAL PRIMARY KEY,         
 	    "title" VARCHAR(200) NOT NULL,
-	    "body" VARCHAR(200) NOT NULL,
+	    "body" VARCHAR(20000) NOT NULL,
         "urllink" VARCHAR(200),
         "date" VARCHAR(100)
     );`;
